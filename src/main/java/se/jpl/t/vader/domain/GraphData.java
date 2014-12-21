bfb51a3315e00cfaba3874e7b4deb6d2ab90e6ad
Fredrik Tuomas <fredrik.tuomas@gmail.com>
2014-12-21 10:40:00 +0100

    Nytt!

---
+++ src/main/java/se/jpl/t/vader/domain/GraphData.java
@@ -0,0 +1,28 @@
+package se.jpl.t.vader.domain;
+
+public class GraphData {
+    String key;
+    Object[][] values;
+    int size = 0;
+    int position = 0;
+
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String series) {
+        this.key = series;
+    }
+    public Object[][] getValues() {
+        return values;
+    }
+
+    public void addPoint(GraphPoint gp) {
+        if (values == null) {
+            values = new Object[100][2];
+            size = 0;
+            position = 0;
+        }
+        values[position] = gp.getPoint();
+        position++;
+    }
+}
